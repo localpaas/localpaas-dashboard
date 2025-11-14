896f87ffb21f0f9ca8b2f7ea723e10b370f2ac4c
DangLe <huudang911998@gmail.com>
2025-11-14 18:29:42 +0700

    fix(header): update header title for user management clarity

--- src/application/modules/users/layouts/main/header/main-header.com.tsx
+++ src/application/modules/users/layouts/main/header/main-header.com.tsx
@@ -15,7 +15,7 @@ export function MainHeader() {
     return (
         <div className="bg-background pt-4 px-5 rounded-lg">
             <div className="py-3 border-b border-border">
-                <h1 className="text-2xl font-bold">Users</h1>
+                <h1 className="text-2xl font-bold">User management</h1>
             </div>
             <TabNavigation links={links} />
         </div>
