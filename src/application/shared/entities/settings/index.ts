d6d834140f4f30d4f01221ebc4a7c0cf78e04453
DangLe <huudang911998@gmail.com>
2026-03-11 22:03:41 +0700

    refactor: remove settings entity definitions for base, notification, and secret.

--- src/application/shared/entities/settings/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./settings.base.entity";
-export * from "./notification/notification.entity";
-export * from "./secret/secret.entity";
--- src/application/shared/entities/settings/secret/secret.entity.ts
+++
@@ -1,7 +0,0 @@
-import { type ESettingType } from "../../../enums";
-import { type SettingsBaseEntity } from '../settings.base.entity';
-
-export interface SecretEntity extends SettingsBaseEntity {
-  type: ESettingType.Secret;
-  key: string;
-}
